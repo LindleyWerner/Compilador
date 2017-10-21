671c3713911648281d44fc8f98aef9108dfb8125
Nícolas Maduro <nicolasamaduro@gmail.com>
2017-10-21 20:57:36 -0200

    Add files via upload

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -3,7 +3,10 @@ package compiler;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lexical_analyzer.Lexer;
+import lexical_analyzer.Semantic;
 import lexical_analyzer.Token;
 
 /**
@@ -13,36 +16,19 @@ import lexical_analyzer.Token;
 public class Compiler {    
     public static void main(String[] args) {
         // TODO chamar tudo por aqui, ou não...
-        Lexer lexer;
         while(true){
             System.out.println("Digite o nome do arquivo a ser compilado: ");
             Scanner input = new Scanner (System.in);
             String fileName = input.nextLine();
             fileName = "./codigosParaTeste/" + fileName;  
-            
+            Semantic semantic;
             try {
-                lexer = new Lexer(fileName);
-                break;
+                semantic = new Semantic(fileName);
+                semantic.start();
             } catch (FileNotFoundException ex) {
-                // TODO Qual o melhor jeito de tratar este erro?               
+                Logger.getLogger(Compiler.class.getName()).log(Level.SEVERE, "Arquivo não encontrado.", ex);
             }
+            
         }
-        System.out.println("\nFluxo de Tokens\n");
-        while(true){
-            try {
-                // Get tokens
-                Token tk = lexer.scan();
-                // If it returns null is end of file
-                if(tk == null) break;
-                
-                System.out.println(tk.toString());
-            } catch (IOException ex) {
-                // TODO Fazer o quê com este erro?
-                System.out.println("Erro");
-                break;
-            }
-        }
-        lexer.howManyErrors();
-        lexer.printTabelaSimbolos();
     }    
 }
